773a5c91325bbe77334cc3582c45ebdcd388d3de
prince475 <danielmwai475@gmail.com>
2023-03-18 23:16:30 +0300

    Completed Validations Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction  Non-Fiction] }
+
+  validate :title_is_clickbaity
+
+
+  def title_is_clickbaity
+    if title == ("True Facts")
+      errors.add(:title, "You are at a wrong title!")
+    end
+  end
 end
